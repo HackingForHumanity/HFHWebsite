efba0ecd47e688b2877b8cf7f07940f8ef4295c6
Dave Balzer <dbalzer@gmail.com>
2015-10-27 15:03:32 -0400

    Initial Check-in of Website Project

---
+++ HackingForHumanity/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HackingForHumanity.MvcApplication" Language="C#" %>
---
+++ HackingForHumanity/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HackingForHumanity.Startup))]
+namespace HackingForHumanity
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
